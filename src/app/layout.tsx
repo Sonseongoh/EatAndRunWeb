a1bb4b3d0fce3be594bbeb816f31d23afa924e40
Sonseongoh <nobleser@naver.com>
2026-04-07 11:41:39 +0900

    구글 애널리틱스 추적 태그 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,20 @@ export default function RootLayout({
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9041574190753656"
           crossOrigin="anonymous"
         />
+        <Script
+          id="google-analytics-script"
+          async
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-0HGF914WGQ"
+        />
+        <Script id="google-analytics-config" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0HGF914WGQ');
+          `}
+        </Script>
         <QueryProvider>
           <AuthProvider>
             <TopNav />
