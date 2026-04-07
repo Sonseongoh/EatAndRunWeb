b577e49093b71612927ddd633d512a38f5084f97
Sonseongoh <nobleser@naver.com>
2026-04-07 12:39:17 +0900

    feat: add Microsoft Clarity tracking script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,6 +55,15 @@ export default function RootLayout({
             gtag('config', 'G-7JRKMDRH7N');
           `}
         </Script>
+        <Script id="microsoft-clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "w7rxyf7yoj");
+          `}
+        </Script>
         <QueryProvider>
           <AuthProvider>
             <TopNav />
